e7b0bc75d27dc7352250e96165ba7baa30147d57
luilom <luigi@LLOMASTO-NB.eustema.intranet>
2019-08-06 11:48:34 +0200

    deleted older files

--- Testing/Utility.py
+++
@@ -1,9 +0,0 @@
-class Result:
-    def __init__(self,label,score):
-        self.label = label
-        self.score = score
-
-class Score:
-    def __init__(self,product,sum):
-        self.product = product
-        self.sum = sum
